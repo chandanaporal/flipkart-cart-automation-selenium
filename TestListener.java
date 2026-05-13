bc827cb7f20874d9823ab3537821ae870fe1ccc6
chandanaporal <166212542+chandanaporal@users.noreply.github.com>
2026-05-13 10:02:53 +0530

    Add files via upload

---
+++ TestListener.java
@@ -0,0 +1,34 @@
+
+package utils;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started : " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test Passed : " + result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed : " + result.getName());
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Execution Started");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Execution Finished");
+    }
+}
